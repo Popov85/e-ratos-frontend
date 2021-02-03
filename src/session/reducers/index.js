017a433f89faed10733c3d961f93e0fb0a9bfe59
Popov85 <andrey.popov85@gmail.com>
2021-02-03 15:30:55 +0200

    Fixed index.js in session module, wrong reducer name

--- src/session/reducers/index.js
+++ src/session/reducers/index.js
@@ -1,11 +1,11 @@
 import {combineReducers} from "redux";
 
 import {regOptionsReducer} from "../../common/reducers/regOptionsReducer";
-import {schemeInfoReducer} from "./SchemeInfoReducer";
 import {sessionReducer} from "./sessionReducer";
 import {logoutReducer} from "../../common/reducers/logoutReducer";
 import {failureReducer} from "./failureReducer";
 import {userReducer} from "../../common/reducers/userReducer";
+import {schemeInfoReducer} from "./schemeInfoReducer";
 
 const rootReducers = combineReducers({
     regOptions: regOptionsReducer,
