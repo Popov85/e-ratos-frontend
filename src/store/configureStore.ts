5b4fef7fd10433862089584eb3dbec998f4f4edb
Popov85 <andrey.popov85@gmail.com>
2024-09-20 13:16:10 +0300

    Disabled serializability check for redux store

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -5,7 +5,9 @@ import rootReducer from "./rootReducer";
 const store = configureStore({
     reducer: rootReducer,  // rootReducer includes all slices (auth, registration, session, etc.)
     devTools: process.env.E_RATOS_PROFILE === "dev",  // Automatically enable Redux DevTools in development
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware(), // Thunk is included by default
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: false,  // Disables the serializability check
+    }), // Thunk is included by default
 });
 
 export default store;
